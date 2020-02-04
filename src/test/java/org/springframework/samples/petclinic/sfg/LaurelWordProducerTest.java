5896a651aa08bb8d194f85cb32bf3cf0ea32d261
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-04 16:24:10 -0600

    Demonstrate very basic Spring test

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/LaurelWordProducerTest.java
@@ -0,0 +1,31 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
+public class LaurelWordProducerTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+//    SPRING WILL HANDLE THESE INSTANTIATIONS NOW THRU @AUTOWIRED
+//    @Before
+//    public void setUp() throws Exception {
+//        hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
+//    }
+
+    @Test
+    public void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+
+        assertEquals("Laurel", word);
+    }
+}
